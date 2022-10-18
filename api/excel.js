3ba7fb8295d0febdb0d3a360bdf5f6535cf57664
SergioFC1989 <sergiofc@hotmail.es>
2022-10-19 01:40:24 +0200

    rename path

--- api/excel.js
+++ api/excel.js
@@ -2,21 +2,21 @@ const XLSX =  require('xlsx');
 
   //For input file
   const onChangeUpload = event => {
-    const fileList = event.target.files
-    for (let i = 0; i < fileList.length; i ++) return (fileList[i])
+    const fileList = event.target.files;
+    for (let i = 0; i < fileList.length; i ++) return (fileList[i]);
   };
 
   //Read all Book
   const onReadBook = async file => {
-    const data = await file.arrayBuffer()
-    const readBookXlsx = XLSX.readFile(data)
-    return readBookXlsx
+    const data = await file.arrayBuffer();
+    const readBookXlsx = XLSX.readFile(data);
+    return readBookXlsx;
   };
 
   //Read only a sheet
   const onReadSheet = (wb, ws) => {
-    const readSheetXlsx = XLSX.utils.sheet_to_json(wb.Sheets[ws])
-    return readSheetXlsx
+    const readSheetXlsx = XLSX.utils.sheet_to_json(wb.Sheets[ws]);
+    return readSheetXlsx;
   };
 
-  module.exports = { onChangeUpload, onReadBook, onReadSheet }
+  module.exports = { onChangeUpload, onReadBook, onReadSheet };
