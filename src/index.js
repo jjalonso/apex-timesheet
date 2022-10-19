eb848179793e389b0d24a45a1d4d26bf808ea0cd
juan_jose_alonso.lara@bp.com <juan_jose_alonso.lara@bp.com>
2022-10-19 02:34:03 +0100

    minor fixes

--- src/index.js
+++ src/index.js
@@ -2,22 +2,24 @@ import prompt from 'prompts';
 
 import { getWeekRange } from './libs/dates.js';
 
-const WEEK_1 = getWeekRange();
-const WEEK_2 = getWeekRange(-1);
-const WEEK_3 = getWeekRange(-2);
-const WEEK_4 = getWeekRange(-3);
+const WEEKS = [
+  getWeekRange(),
+  getWeekRange(-1),
+  getWeekRange(-2),
+  getWeekRange(-3)
+];
 
-const DATE_DISPLAY_FORMAT = 'DD/MM/YYYY';
+const DATE_DISPLAY_FORMAT = 'Do MMM/YYYY';
 
 const questions = [
     {
       type: 'select',
       name: 'week',
       message: 'Pick a week',
-      choices: [WEEK_1, WEEK_2, WEEK_3, WEEK_4].map((week, index) => ({
+      choices: WEEKS.map((week, i) => ({
           title: `${week[0].format(DATE_DISPLAY_FORMAT)} > ${week[6].format(DATE_DISPLAY_FORMAT)}`,
           value: week,
-          description: index === 0 ? '* Current week' : undefined
+          description: i === 0 ? '* Current week' : undefined
       }))
     },
     {
@@ -28,4 +30,4 @@ const questions = [
     },
 ];
 
-const answers = await prompt(questions, {onCancel:() => {}, onSubmit:() => {}});
+const answers = await prompt(questions, {onSubmit:() => console.log('---> TODO: Send Email')});
