deeb8128b926b97d1e941878ff3b5032eda100b5
HerbaDev <sfernandez@herba.es>
2022-10-21 14:12:06 +0200

    fix functions excel

--- src/libs/excel.cjs
+++ src/libs/excel.cjs
@@ -1,15 +1,19 @@
 const Excel = require('exceljs');
-const workbook = new Excel.Workbook();
 
-const handleReadFile = wbName => workbook.xlsx.readFile(wbName)
-
-const handleWriteFile = ({number_sheet = 1, number_row = 1, number_cell = 1, value = ''}) => {
+const addValueInWorksheeet = async ({ 
+	workbook_name, 
+	number_sheet, 
+	number_row, 
+	number_cell, 
+	value 
+}) => {
+	const excel = new Excel.Workbook()
+	const workbook = await excel.xlsx.readFile(workbook_name)
 	const worksheet = workbook.getWorksheet(number_sheet)
 	const row = worksheet.getRow(number_row)
 	row.getCell(number_cell).value = value
-	row.commit();
+	row.commit()
+	return workbook.xlsx.writeFile(workbook_name)
 };
 
-const handleCreateWorkbook = name => workbook.xlsx.writeFile(name);
-
-module.exports = { handleReadFile, handleWriteFile, handleCreateWorkbook };
+module.exports = { addValueInWorksheeet };
--- src/utils/output-file.cjs
+++ src/utils/output-file.cjs
@@ -1,7 +1,9 @@
-const { handleReadFile, handleWriteFile, handleCreateWorkbook } = require('../libs/excel.cjs');
+const { addValueInWorksheeet } = require('../libs/excel.cjs');
 
-const addValue = async () => {
-  await handleReadFile('base.xlsx')
-  handleWriteFile({ number_sheet: 1, number_row: 1, number_cell: 2, value: 'Prueba manuela' })
-  handleCreateWorkbook('base.xlsx')
-};
+addValueInWorksheeet({ 
+  workbook_name: 'base.xlsx',
+  number_sheet: 1, 
+  number_row: 1, 
+  number_cell: 2, 
+  value: 'sergio' 
+});
